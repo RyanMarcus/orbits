7d02860134c84afa00e880adfdab37b8e25eb988
Ryan Marcus <ryan@rmarcus.info>
2016-07-28 02:13:49 -0400

    formatting

--- test/decomp_tests.js
+++ test/decomp_tests.js
@@ -21,30 +21,30 @@ var orbit = require("../orbits");
 
 
 describe("transportation decomposition", function () {
-	it("should decompose an arbitrary permutation", function() {
-		var p1 = {"a": "b", "b": "c", "c": "a"};
-		var xps = orbit.transpositionDecomposition(p1);
+    it("should decompose an arbitrary permutation", function() {
+	var p1 = {"a": "b", "b": "c", "c": "a"};
+	var xps = orbit.transpositionDecomposition(p1);
 
-		assert.equal(xps.length, 2);
-		assert.equal(xps[0][0], 'a');
-		assert.equal(xps[0][1], 'b');
-		assert.equal(xps[1][0], 'b');
-		assert.equal(xps[1][1], 'c');
+	assert.equal(xps.length, 2);
+	assert.equal(xps[0][0], 'a');
+	assert.equal(xps[0][1], 'b');
+	assert.equal(xps[1][0], 'b');
+	assert.equal(xps[1][1], 'c');
 
-	});
-
-
-	it("should preserve a complex permutation", function () {
-		var p1 = {"a": "b", "b": "c", 
-			  "c": "d", "d": "a",
-			  "f": "g", "g": "f"};
-
-		var xps = orbit.transpositionDecomposition(p1);
-		
-		xps = xps.map(orbit.cycleToPerm)
-			 .reduce(orbit.composePermutations);
+    });
 
 
-		assert(orbit.permutationsEqual(p1, xps));
-	});
+    it("should preserve a complex permutation", function () {
+	var p1 = {"a": "b", "b": "c", 
+		  "c": "d", "d": "a",
+		  "f": "g", "g": "f"};
+
+	var xps = orbit.transpositionDecomposition(p1);
+	
+	xps = xps.map(orbit.cycleToPerm)
+	    .reduce(orbit.composePermutations);
+
+
+	assert(orbit.permutationsEqual(p1, xps));
+    });
 });
